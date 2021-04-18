edd66417d7f699a860b47568aeadcddbd33386b1
isaac k. kimura <37881972+KKaranja@users.noreply.github.com>
2021-04-18 03:30:31 +0300

    diplay nav bar according to  devices, added the mobile hamburger menu aninamtion.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello there!");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("openHamburger");
+
+  if (header.classList.contains("open")) {
+    //close Hamburger menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    //open Hamburger menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
